8a5bc93b55ec8b1ac1d6b59611c2206135fa09a0
Khabab Akhtar <khababakhtar20@gmail.com>
2025-06-04 00:41:07 +0500

    Time: 3 ms (39.43%) | Memory: 17.6 MB (90.94%) - LeetSync

---
+++ 3324-split-the-array/split-the-array.py
@@ -0,0 +1,18 @@
+class Solution:
+    def isPossibleToSplit(self, nums: List[int]) -> bool:
+        freq = {}
+        for num in nums:
+            if num in freq:
+                freq[num]+=1
+            else:
+                freq[num] = 1
+
+        for key in nums:
+            if freq[key] > 2:
+                return False
+
+        return True
+
+
+
+        
